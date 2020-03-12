1fe9f111ec759109bb6d65c4dbdba711004e6802
Eric Golde <eric@golde.org>
2020-03-11 23:41:28 -0700

    recipe changes for ore dictionary

--- src/main/java/org/golde/enhancedvanilla/proxy/CommonProxy.java
+++ src/main/java/org/golde/enhancedvanilla/proxy/CommonProxy.java
@@ -4,18 +4,21 @@ import org.golde.enhancedvanilla.EnhancedVanilla;
 import org.golde.enhancedvanilla.gui.ForgeGuiHandler;
 import org.golde.enhancedvanilla.init.BDispenserFunctions;
 
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
+import net.minecraftforge.oredict.OreDictionary;
 
 @Mod.EventBusSubscriber
 public class CommonProxy {
 
 	public void preInit(FMLPreInitializationEvent e) {
-		
+		OreDictionary.registerOre("wool", new ItemStack(Blocks.WOOL, 1, OreDictionary.WILDCARD_VALUE));
 	}
 	
 	public void init(FMLInitializationEvent e) {
